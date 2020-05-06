a9db2d0ce7593609479f36272e395625afa66b18
Kazuki Minakawa <k.minakawa@playree.com>
2020-05-06 12:37:12 +0900

    add Accordion

--- README.md
+++ README.md
@@ -3,6 +3,11 @@ CSS Framework
 
 # 開発用
 
+## テスト表示
+```
+hugo server
+```
+
 ## 公開用ファイル生成
 ```
 hugo -d ./docs
--- static/js/kuroutokit.js
+++ static/js/kuroutokit.js
@@ -1,2 +1,21 @@
 /*! KUROUTO kit 0.0.1 | https:// | (c) 2020 Kazuki Minakawa (funlab, Inc.) | MIT License */
 
+document.addEventListener('DOMContentLoaded', () => {
+    kkReady();
+});
+
+function kkReady() {
+    document.addEventListener("click", (event) => {
+        console.log(event);
+        if(event.target.classList.contains("kk-accordion")) {
+            const ac = event.target.nextElementSibling;
+            if(event.target.hasAttribute("closed")) {
+                ac.style.height = ac.scrollHeight + "px";
+                event.target.removeAttribute("closed");
+            } else {
+                ac.style.height = 0;
+                event.target.setAttribute("closed", "");
+            }
+        }
+    }, false);
+}
