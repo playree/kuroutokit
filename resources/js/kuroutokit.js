54e7ce6e8bc8da0e45957460744e6de610e9cf58
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2021-05-29 11:56:52 +0900

    Modalを追加

--- resources/js/kuroutokit.js
+++ resources/js/kuroutokit.js
@@ -1,11 +1,11 @@
 /*! KUROUTO kit 0.0.2 | https://playree.github.io/kuroutokit/ | (c) 2020 Kazuki Minakawa (funlab, Inc.) | MIT License */
 
 (function (global, factory) {
-    typeof exports === "object" && typeof module !== "undefined" ? module.exports = factory() :
-    typeof define === "function" && define.amd ? define("kuroutokit", factory) :
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define('kuroutokit', factory) :
     (global = global || self, global.KuroutoKit = factory());
-}(this, (function () { "use strict";
-    document.addEventListener("DOMContentLoaded", () => {
+}(this, (function () { 'use strict';
+    document.addEventListener('DOMContentLoaded', () => {
         loaded();
     });
 
@@ -21,14 +21,16 @@ let kk_initInput = false;
 let kk_initMovableLabel = false;
 let kk_initTab = false;
 let kk_initEnableWithCheck = false;
+let kk_initModal = false;
+let kk_modalArgs = null;
 
 function _initTab() {
-    [].forEach.call(document.getElementsByClassName("kk-tab"), (el) => {
-        const view = document.getElementById(el.getAttribute("for"))
+    [].forEach.call(document.getElementsByClassName('kk-tab'), (el) => {
+        const view = document.getElementById(el.getAttribute('for'))
         if(el.checked){
-            view.removeAttribute("hidden");
+            view.removeAttribute('hidden');
         } else {
-            view.setAttribute("hidden", "");
+            view.setAttribute('hidden', '');
         }
     });
 }
@@ -38,23 +40,23 @@ class KuroutoKit {
         if(kk_initAccordion) return;
         kk_initAccordion = true;
 
-        document.addEventListener("click", (event) => {
+        document.addEventListener('click', (event) => {
             // Accordion.
-            if(event.target.classList.contains("kk-accordion")) {
+            if(event.target.classList.contains('kk-accordion')) {
                 const ac = event.target.nextElementSibling;
-                if(event.target.hasAttribute("closed")) {
-                    ac.style.height = ac.scrollHeight + "px";
-                    event.target.removeAttribute("closed");
+                if(event.target.hasAttribute('closed')) {
+                    ac.style.height = ac.scrollHeight + 'px';
+                    event.target.removeAttribute('closed');
                 } else {
                     ac.style.height = 0;
-                    event.target.setAttribute("closed", "");
+                    event.target.setAttribute('closed', '');
                 }
             }
         }, false);
-        [].forEach.call(document.getElementsByClassName("kk-accordion"), (el) => {
-            if(!el.hasAttribute("closed")) {
+        [].forEach.call(document.getElementsByClassName('kk-accordion'), (el) => {
+            if(!el.hasAttribute('closed')) {
                 const ac = el.nextElementSibling;
-                ac.style.height = ac.scrollHeight + "px";
+                ac.style.height = ac.scrollHeight + 'px';
             }
         });
     }
@@ -63,17 +65,17 @@ class KuroutoKit {
         if(kk_initInput) return;
         kk_initInput = true;
 
-        [].forEach.call(document.getElementsByTagName("INPUT"), (el) => {
-            if(el.hasAttribute("invalid")) {
-                el.addEventListener("focus", (event) => {
-                    el.removeAttribute("invalid")
+        [].forEach.call(document.getElementsByTagName('INPUT'), (el) => {
+            if(el.hasAttribute('invalid')) {
+                el.addEventListener('focus', (event) => {
+                    el.removeAttribute('invalid')
                 }, {once:true});
             }
         });
-        [].forEach.call(document.getElementsByTagName("TEXTAREA"), (el) => {
-            if(el.hasAttribute("invalid")) {
-                el.addEventListener("focus", (event) => {
-                    el.removeAttribute("invalid")
+        [].forEach.call(document.getElementsByTagName('TEXTAREA'), (el) => {
+            if(el.hasAttribute('invalid')) {
+                el.addEventListener('focus', (event) => {
+                    el.removeAttribute('invalid')
                 }, {once:true});
             }
         });
@@ -83,28 +85,28 @@ class KuroutoKit {
         if(kk_initMovableLabel) return;
         kk_initMovableLabel = true;
 
-        document.addEventListener("change", (event) => {
-            if(event.target.tagName == "INPUT" || event.target.tagName == "TEXTAREA") {
-                if(event.target.parentElement.classList.contains("kk-input-field")) {
-                    if(event.target.value == "") {
-                        event.target.removeAttribute("kk-active");
+        document.addEventListener('change', (event) => {
+            if(event.target.tagName == 'INPUT' || event.target.tagName == 'TEXTAREA') {
+                if(event.target.parentElement.classList.contains('kk-input-field')) {
+                    if(event.target.value == '') {
+                        event.target.removeAttribute('kk-active');
                     } else {
-                        event.target.setAttribute("kk-active", "");
+                        event.target.setAttribute('kk-active', '');
                     }
                 }
             }
         }, false);
-        [].forEach.call(document.getElementsByTagName("INPUT"), (el) => {
-            if(el.parentElement.classList.contains("kk-input-field")) {
-                if(el.value != "") {
-                    el.setAttribute("kk-active", "");
+        [].forEach.call(document.getElementsByTagName('INPUT'), (el) => {
+            if(el.parentElement.classList.contains('kk-input-field')) {
+                if(el.value != '') {
+                    el.setAttribute('kk-active', '');
                 }
             }
         });
-        [].forEach.call(document.getElementsByTagName("TEXTAREA"), (el) => {
-            if(el.parentElement.classList.contains("kk-input-field")) {
-                if(el.value != "") {
-                    el.setAttribute("kk-active", "");
+        [].forEach.call(document.getElementsByTagName('TEXTAREA'), (el) => {
+            if(el.parentElement.classList.contains('kk-input-field')) {
+                if(el.value != '') {
+                    el.setAttribute('kk-active', '');
                 }
             }
         });
@@ -114,9 +116,9 @@ class KuroutoKit {
         if(kk_initTab) return;
         kk_initTab = true;
 
-        document.addEventListener("change", (event) => {
-            if(event.target.tagName == "INPUT") {
-                if(event.target.classList.contains("kk-tab")) {
+        document.addEventListener('change', (event) => {
+            if(event.target.tagName == 'INPUT') {
+                if(event.target.classList.contains('kk-tab')) {
                     _initTab()
                 }
             }
@@ -128,53 +130,109 @@ class KuroutoKit {
         if(kk_initEnableWithCheck) return;
         kk_initEnableWithCheck = true;
 
-        document.addEventListener("change", (event) => {
-            if(event.target.tagName == "INPUT") {
-                if(event.target.classList.contains("kk-enable-with-check")) {
-                    const targets = event.target.getAttribute("target").split(",");
+        document.addEventListener('change', (event) => {
+            if(event.target.tagName == 'INPUT') {
+                if(event.target.classList.contains('kk-enable-with-check')) {
+                    const targets = event.target.getAttribute('target').split(',');
                     [].forEach.call(targets, (target) => {
                         if(event.target.checked) {
-                            document.getElementById(target.trim()).removeAttribute("disabled");
+                            document.getElementById(target.trim()).removeAttribute('disabled');
                         } else {
-                            document.getElementById(target.trim()).setAttribute("disabled", "");
+                            document.getElementById(target.trim()).setAttribute('disabled', '');
                         }
                     });
-                } else if(event.target.classList.contains("kk-disable-with-check")) {
-                    const targets = event.target.getAttribute("target").split(",");
+                } else if(event.target.classList.contains('kk-disable-with-check')) {
+                    const targets = event.target.getAttribute('target').split(',');
                     [].forEach.call(targets, (target) => {
                         if(event.target.checked) {
-                            document.getElementById(target.trim()).setAttribute("disabled", "");
+                            document.getElementById(target.trim()).setAttribute('disabled', '');
                         } else {
-                            document.getElementById(target.trim()).removeAttribute("disabled");
+                            document.getElementById(target.trim()).removeAttribute('disabled');
                         }
                     });
                 }
             }
         }, false);
-        [].forEach.call(document.getElementsByTagName("INPUT"), (el) => {
-            if(el.classList.contains("kk-enable-with-check")) {
-                if(el.hasAttribute("target")) {
-                    const targets = el.getAttribute("target").split(",");
+        [].forEach.call(document.getElementsByTagName('INPUT'), (el) => {
+            if(el.classList.contains('kk-enable-with-check')) {
+                if(el.hasAttribute('target')) {
+                    const targets = el.getAttribute('target').split(',');
                     [].forEach.call(targets, (target) => {
                         if(el.checked) {
-                            document.getElementById(target.trim()).removeAttribute("disabled");
+                            document.getElementById(target.trim()).removeAttribute('disabled');
                         } else {
-                            document.getElementById(target.trim()).setAttribute("disabled", "");
+                            document.getElementById(target.trim()).setAttribute('disabled', '');
                         }
                     });
                 }
-            } else if(el.classList.contains("kk-disable-with-check")) {
-                if(el.hasAttribute("target")) {
-                    const targets = el.getAttribute("target").split(",");
+            } else if(el.classList.contains('kk-disable-with-check')) {
+                if(el.hasAttribute('target')) {
+                    const targets = el.getAttribute('target').split(',');
                     [].forEach.call(targets, (target) => {
                         if(el.checked) {
-                            document.getElementById(target.trim()).setAttribute("disabled", "");
+                            document.getElementById(target.trim()).setAttribute('disabled', '');
                         } else {
-                            document.getElementById(target.trim()).removeAttribute("disabled");
+                            document.getElementById(target.trim()).removeAttribute('disabled');
                         }
                     });
                 }
             }
         });
+    }
+
+    static initModal() {
+        if(kk_initModal) return;
+        kk_initModal = true;
+
+        var body_element = document.getElementsByTagName('body')[0];
+        body_element.insertAdjacentHTML('beforeend',
+            '<div id="kk_modal" class="kk-modal" closed><div id="kk_modal_inner">' + 
+                '<div id="kk_modal_title" class="kk-border-bottom kk-font-bold kk-padding-ss"></div>' +
+                '<div id="kk_modal_contents" class="kk-margin-top kk-padding-s"></div>' +
+                '<div class="kk-margin-top kk-padding" grid-col>' + 
+                    '<div class="kk-col5"><button id="kk_modal_no" class="kk-button" onclick="KuroutoKit.onModalNo();"></button></div>' +
+                    '<div class="kk-col2"></div>' +
+                    '<div class="kk-col5"><button id="kk_modal_yes" theme="primary" class="kk-button" onclick="KuroutoKit.onModalYes();"></button></div>' +
+                '</div>' +
+            '</div></div>');
+        
+        document.getElementById('kk_modal').addEventListener('click', (e) => {
+            KuroutoKit.closeModal();
+        });
+        document.getElementById('kk_modal_inner').addEventListener('click', (e) => {
+            e.stopPropagation();
+        });
+    }
+
+    static openModal(args) {
+        kk_modalArgs = args;
+        document.getElementById('kk_modal_title').innerHTML = kk_modalArgs.title;
+        document.getElementById('kk_modal_contents').innerHTML = kk_modalArgs.contents;
+        if ('yes_str' in kk_modalArgs) {
+            document.getElementById('kk_modal_yes').innerHTML = kk_modalArgs.yes_str;
+        } else {
+            document.getElementById('kk_modal_yes').innerHTML = 'はい';
+        }
+        if ('no_str' in kk_modalArgs) {
+            document.getElementById('kk_modal_no').innerHTML = kk_modalArgs.no_str;
+        } else {
+            document.getElementById('kk_modal_no').innerHTML = 'いいえ';
+        }
+        document.getElementById('kk_modal').removeAttribute('closed');
+    }
+    static closeModal() {
+        document.getElementById('kk_modal').setAttribute('closed', '');
+    }
+    static onModalYes() {
+        if ('yes_func' in kk_modalArgs) {
+            kk_modalArgs.yes_func();
+        }
+    }
+    static onModalNo() {
+        if ('no_func' in kk_modalArgs) {
+            kk_modalArgs.no_func();
+        } else {
+            KuroutoKit.closeModal();
+        }
     }
 }
